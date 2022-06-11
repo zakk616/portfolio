cf7ba0c002e04972a0285d6b41584e63d8046a48
Muhammad Zakaria <muhammadzakaria616@gmail.com>
2022-06-11 10:58:30 +0500

    some modifications

--- src/components/skills.js
+++ src/components/skills.js
@@ -5,7 +5,30 @@ export default function Skills() {
   return (
     <section id="skills">
       <div className="container">
-        <SkillSection heading="Languages" />
+        {/* <SkillSection heading="Languages" /> */}
+        <div className="row justify-content-center">
+          <h2 className="skill-title">Languages</h2>
+        </div>
+        <div className="row justify-content-center">
+          <div className="skill-icon col-sm-6 col-md-6 col-lg-2">
+            <i className="devicon-cplusplus-plain-wordmark colored"></i>
+          </div>
+          <div className="skill-icon col-sm-6 col-md-6 col-lg-2">
+            <i className="devicon-csharp-plain-wordmark colored"></i>
+          </div>
+          <div className="skill-icon col-sm-6 col-md-6 col-lg-2">
+            <i className="devicon-python-plain-wordmark colored"></i>
+          </div>
+          <div className="skill-icon col-sm-6 col-md-6 col-lg-2">
+            <i className="devicon-html5-plain-wordmark colored"></i>
+          </div>
+          <div className="skill-icon col-sm-6 col-md-6 col-lg-2">
+            <i className="devicon-css3-plain-wordmark colored"></i>
+          </div>
+          <div className="skill-icon col-sm-6 col-md-6 col-lg-2">
+            <i className="devicon-javascript-plain colored"></i>
+          </div>
+        </div>
         <hr />
         <div className="row justify-content-center">
           <h2 className="skill-title">Frameworks</h2>
