3caf948a11474a622072dc81ee94b23722765499
Muhammad Zakaria <muhammadzakaria616@gmail.com>
2022-06-24 23:19:30 +0500

    js particle added

--- src/components/cover.js
+++ src/components/cover.js
@@ -1,40 +1,107 @@
 import "../styles.css";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import ExampleDoc from "../../Dotnet_CV.pdf";
 
 export default function Cover() {
+  const particlesInit = async (main) => {
+    console.log(main);
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <div id="particles-js">
-      <div id="js-dim"></div>
-      <a id="js-download" className="download" href="#"></a>
-      <div className="cv-role" id="cv-role">
-        <a
-          href="./public/cv-dotnet.pdf"
-          className="js-dismiss"
-          data-os="Dotnet FullStack"
-        >
-          <img
-            className="img-icon"
-            src="images/img-dotnet.svg"
-            alt="dotnet icon"
-          />
-        </a>
-        <a
-          href="./public/cv-ml.pdf"
-          className="js-dismiss"
-          data-os="Machine Learning"
-        >
-          <img
-            className="img-icon"
-            src="images/img-tensorflow.svg"
-            alt="tensorflow icon"
-          />
-        </a>
-      </div>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push"
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse"
+              },
+              resize: true
+            },
+            modes: {
+              push: {
+                quantity: 4
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4
+              }
+            }
+          },
+          particles: {
+            color: {
+              value: "#ffffff"
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1
+            },
+            collisions: {
+              enable: true
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce"
+              },
+              random: false,
+              speed: 1,
+              straight: false
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800
+              },
+              value: 80
+            },
+            opacity: {
+              value: 0.5
+            },
+            shape: {
+              type: "circle"
+            },
+            size: {
+              value: { min: 1, max: 5 }
+            }
+          },
+          detectRetina: true
+        }}
+      />
+
+      <a
+        id="js-download"
+        className="download"
+        href={ExampleDoc}
+        target="_blank"
+      ></a>
 
       <div className="header">
         <h1>
           <span className="site-title">MUHAMMAD ZAKARIA</span>
           <div className="site-description">
-            <p id="typed" className="typed"></p>
+            <p id="typed" className="typed">
+              Dotnet Developer
+            </p>
           </div>
         </h1>
 
